cc8796bc9531b4fe0331a6a335a7e4a30d124063
zqunor <zqunor@gmail.com>
2020-04-17 02:54:54 +0000

    封装业务代码 - 购物车service

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,15 +8,16 @@ use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
+use App\Services\CartService;
 use Carbon\Carbon;
 
 class OrdersController extends Controller
 {
-    public function store(OrderRequest $request)
+    public function store(OrderRequest $request, CartService $cartService)
     {
         $user = $request->user();
         // 开启一个事务
-        $order = \DB::transaction(function () use ($user, $request) {
+        $order = \DB::transaction(function () use ($user, $request, $cartService) {
             $address = UserAddress::find($request->input('address_id'));
             // 更新地址的最新使用时间
             $address->update(['last_used_at' => Carbon::now()]);
@@ -62,7 +63,7 @@ class OrdersController extends Controller
 
             // 将下单的商品从购物车中移除
             $skuIds = collect($items)->pluck('sku_id');
-            $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
+            $cartService->remove($skuIds);
 
             // 创建
             $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
