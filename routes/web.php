982a71f24f4cf0561f17af1c17dbe493abf7a5ee
zqunor <zqunor@gmail.com>
2020-05-06 18:49:50 +0800

    用户界面 - 检查优惠券 1、优惠券使用场景 - 购物车下单时通过优惠码检查是否存在优惠

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
+
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 });
 
 // 支付的服务器请求不会带有认证信息
