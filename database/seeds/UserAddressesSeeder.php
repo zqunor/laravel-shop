f745c0d649226781e65cb5bce0c91b19125454ed
zqunor <zqunor@gmail.com>
2020-05-19 19:26:24 +0800

    完善假数据填充 1、Seeder类调用逻辑  - 创建工厂类 - 生成随机假数据  - 创建Seeder类 - 按需设置假数据规模  - Seeder 类的自动加载  - database/seeds/DatabaseSeeder.php 2、订单假数据  - 订单工厂类 - 用户/收货地址/优惠券  - 订单明细工厂类 - 商品spu/商品sku  - 创建Seeder类 - 生成订单、订单明细后，统计订单数据（销量、评分、评价数）

---
+++ database/seeds/UserAddressesSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserAddressesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::all()->each(function (User $user) {
+            factory(UserAddress::class, random_int(1, 3))->create(['user_id' => $user->id]);
+        });
+    }
+}
